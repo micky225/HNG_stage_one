2cceb80989e9250411ad3555d21548a766cbcf55
Code_X <mickytod2255@gmail.com>
2023-09-09 21:54:33 -0700

    Stage One

---
+++ Endpoint/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+import datetime
+import pytz
+from django.http import JsonResponse
+from django.utils import timezone
+# Create your views here.
+
+def endpoint(request):
+    slack_name = request.GET.get('slack_name', 'Michael')
+    track = request.GET.get('track', 'Backend')
+
+    # Current Week
+    current_day = datetime.datetime.now().strftime("%A")
+
+    # Current UTC time
+    current_utc_time = timezone.now()
+    utc_time_format = "%Y-%m-%dT%H:%M:%SZ"
+
+    # Github file url
+    github_file_url = "https://github.com/micky225/HNG_stage_one"
+    github_repo_url = ...
+   
+
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": current_utc_time.strftime(utc_time_format),
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_ur,
+        "status_code": 200
+    }
+
+    return JsonResponse(response_data)
