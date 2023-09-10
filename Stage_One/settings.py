5660ea25a5c15eead6cb390ea6a8d1bd7dc61b2f
Code_X <mickytod2255@gmail.com>
2023-09-09 23:35:19 -0700

    allow host

--- Stage_One/settings.py
+++ Stage_One/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-70_n=*eojt#4dc+i#7-6np5w!08e0ei*ayz3&xz65!^hg9f2f%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
